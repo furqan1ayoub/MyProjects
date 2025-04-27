8151deb6243c6561ba2150f5190e0363bba00535
furqanAyoub <furqanayoubmir@gmail.com>
2025-04-27 21:40:53 +0530

    -

--- os-utilty/main.py
+++
@@ -1,9 +0,0 @@
-#This is a program which has options like 
-#1) delte temp files
-#2)detect current os (use platform)
-#3)get all current env variable
-#4)specific env variable
-#5)check files specific in any path 
-#6) check any extension in any path 
-#7) organize the .txt .csv or any text into Folders quickly in few steps
-#8)move / remove files 
